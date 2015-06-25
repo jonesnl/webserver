6971d6388023fad19a3377e6d556d73ef7f913df
Nate Jones <jonesnl@umich.edu>
2015-06-25 00:12:22 -0400

    Sending hardcoded HTML to browser, and that works!

---
+++ Makefile
@@ -0,0 +1,20 @@
+#Default Flags
+FLAGS = -Wall -Wextra -Wconversion -pedantic -Wvla -std=c++11
+
+# make release - will compile "all" with $(FLAGS) and the O3 flag
+release: FLAGS += -O3
+release: all
+
+# make debug - will compile "all" with $(FLAGS) and the g flag
+debug: FLAGS += -g
+debug: all
+
+profile: FLAGS += -pg
+profile: all
+
+all : server.cpp
+	g++ $(FLAGS) -o out server.cpp 
+
+# make clean - remove .o files.  You can modify it to remove whatever
+clean:
+	rm -f *.o out
--- server.cpp
+++ server.cpp
@@ -1,11 +1,14 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <netinet/in.h>
 
 #include <iostream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -27,14 +30,16 @@ int main() {
     listen(accept_sock, 5);
 
     while (true) {
-        char rec_buf[10000];
+        char rec_buf[10000], send_buf[10000], file_buf[10000];
         memset(rec_buf, 0, sizeof(rec_buf));
+        memset(send_buf, 0, sizeof(send_buf));
+        memset(file_buf, 0, sizeof(file_buf));
         conn_sock = accept(accept_sock, NULL, NULL);
         if (conn_sock == -1) return -1;
         cout << "Accepted connection\n";
         
         // Read HTTP request
-        int end = 0;
+        ssize_t end = 0;
         char *end_of_header = NULL;
         while((end_of_header = strstr(rec_buf, "\r\n\r\n")) == NULL) {
             end += recv(conn_sock, rec_buf + end, sizeof(rec_buf) - end - 1, 0);
@@ -47,8 +52,42 @@ int main() {
         // size_t buf_len = end_of_header - rec_buf + 4;
 
         cout << rec_buf << endl;
+
+        // Get content from file
+        // TODO: Change hardcoded filename
+        char *file_end = file_buf;
+        int fd = open("./sample_html.html", O_RDONLY);
+        if (fd == -1) return -1;
+        ssize_t ret;
+        do {
+            ret = read(fd, file_end, sizeof(file_buf) - (file_end-file_buf));
+            file_end += ret;
+        } while (ret != 0 && ret != -1);
+        
+        long content_length = file_end - file_buf;
+        string cont_len = to_string(content_length);
+
+        // Store HTTP response header in buf
+        char *headerptr = send_buf;
+        // TODO set Content-Type on the fly
+        char first_line[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
+        memcpy(headerptr, first_line, sizeof(first_line) - 1);
+        headerptr += sizeof(first_line) - 1;
+        memcpy(headerptr, cont_len.c_str(), cont_len.size());
+        headerptr += cont_len.size();
+        memcpy(headerptr, "\r\n\r\n", 4);
+        headerptr += 4;
+        
+        // Store content in buf
+        memcpy(headerptr, file_buf, content_length);
+        headerptr += content_length;
+        cout << send_buf << endl;
+        char* sendptr = send_buf;
+
+        // Send buf back to browser
+        while (sendptr != headerptr) {
+            sendptr += send(conn_sock, sendptr, headerptr - sendptr, 0);
+        }
         close(conn_sock);
-        // Give HTTP response header
-        // Give Theora video
     }
 }

ecd60856bd6356a3c92856f7d9acb1fa91cf9240
Nate Jones <jonesnl@umich.edu>
2015-07-11 23:31:50 -0400

    Update server
    Add a bit of MIME parsing, parse requested file location from HTTP
    header, update sample webpage with HTML5 video tag. All working.

--- server.cpp
+++ server.cpp
@@ -4,8 +4,32 @@
 #include <cstring>
 #include <string>
 #include <sstream>
+#include <regex>
 
 #include <boost/asio.hpp>
+
+std::string parse_http_header(std::string header) {
+    std::regex regex("^GET (/\\S*) HTTP/1\\.1\r\n");
+    std::smatch results;
+    bool ret = std::regex_search(header, results, regex);
+    if (ret && results[1].str().find("..") == std::string::npos) {
+        return results[1];
+    } else {
+        return std::string();
+    }
+}
+
+std::string mime_type(std::string file_name) {
+    std::regex regex("\\S+\\.(\\S+)$");
+    std::smatch result;
+    bool ret = std::regex_match(file_name, result, regex);
+    if (ret) {
+        std::string extension = result[1];
+        if (extension == "html") return "text/html";
+        else if (extension == "ogv") return "video/ogg";
+        else return "text/html";
+    } else return "text/html";
+}
 
 int main() {
     boost::asio::io_service io_service;
@@ -32,15 +56,22 @@ int main() {
 
         std::cout << s << std::endl;
 
-        std::ifstream fstr("./sample_html.html");
+        // Parse request
+        std::string file_name = parse_http_header(s);
+        std::string m_type = mime_type(file_name);
+
+        // Get data from file
+        std::ifstream fstr("." + file_name);
         std::ostream file_stream(&filebuf);
         file_stream << fstr.rdbuf();
         fstr.close();
 
-
+        // Create response header
         std::ostream header_stream(&headerbuf);
-        header_stream << "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
-        header_stream << filebuf.size() << "\r\n\r\n";
+        header_stream << "HTTP/1.1 200 OK\r\n"
+            << "Content-Type: " << m_type << "\r\n"
+            << "Content-Length: " << filebuf.size() << "\r\n"
+            << "\r\n";
 
         s = std::string(
                 boost::asio::buffers_begin(headerbuf.data()),
@@ -54,6 +85,7 @@ int main() {
 
         std::cout << s << std::endl;
 
+        // Send header and file back to requester
         boost::asio::write(socket, headerbuf);
         boost::asio::write(socket, filebuf);
         socket.close();
